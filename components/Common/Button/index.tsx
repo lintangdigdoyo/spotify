ca232684ccf02d01b5035e68ae21ec186e7018e9
lintang digdoyo <digdoyo.lintang@gmail.com>
2023-04-06 18:55:48 +0700

    feat: add button variant

--- components/Common/Button/index.tsx
+++ components/Common/Button/index.tsx
@@ -1,31 +1,43 @@
 import cnMerge from "utils/cnMerge";
 
+import { VariantType } from "./types";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
+  variant?: VariantType;
   children: React.ReactNode;
 }
 
-const Button = ({ className, children, ...restProps }: ButtonProps) => {
+const Button = ({
+  className,
+  variant = "contained",
+  children,
+  ...restProps
+}: ButtonProps) => {
   return (
     <button
       className={cnMerge(
         /** base style */
         "relative h-10 w-fit overflow-hidden whitespace-nowrap rounded-md",
-        "border border-primary-main px-6 font-semibold text-primary-main transition-all",
+        "border border-primary-main px-6 font-semibold transition-all",
+        /** variants */
+        {
+          ["text-primary-main before:-translate-x-full after:translate-x-full hover:text-white hover:before:-translate-x-[30%] hover:after:translate-x-[30%]"]:
+            variant === "outlined",
+          ["text-white hover:text-primary-main hover:before:-translate-x-full hover:after:translate-x-full"]:
+            variant === "contained",
+        },
         /** after */
         "after:absolute after:inset-x-0 after:inset-y-0 after:-z-[1]",
-        "after:translate-x-full",
         "after:border-y-[20px] after:border-l-[20px] after:border-primary-main after:border-l-transparent",
         "after:transition-all after:duration-300 after:content-['']",
         /** before */
         "before:absolute before:inset-x-0 before:inset-y-0 before:-z-[1]",
-        "before:-translate-x-full",
         "before:border-y-[20px] before:border-r-[20px] before:border-primary-main before:border-r-transparent",
         "before:transition-all before:duration-300 before:content-['']",
-        /** hover */
-        "hover:text-white hover:before:-translate-x-[30%] hover:after:translate-x-[30%]",
         /** active */
         "active:scale-95 active:brightness-75",
+        /** custom styles */
         className
       )}
       {...restProps}
---
+++ components/Common/Button/types.ts
@@ -0,0 +1,1 @@
+export type VariantType = "outlined" | "contained";
