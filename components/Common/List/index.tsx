421d41d5dd2370dc4f1b75445efb6378348ef30a
lintang digdoyo <digdoyo.lintang@gmail.com>
2023-04-07 21:57:54 +0700

    feat: update bullet list size

--- components/Common/List/index.tsx
+++ components/Common/List/index.tsx
@@ -17,6 +17,7 @@ const List = ({ genres, activeIndex }: ListProps) => {
             style={
               {
                 "--brightness-percentage": activeIndex === index ? 1 : 0.5,
+                "--bullet-size": activeIndex === index ? "14px" : "13px",
               } as CSSProperties
             }
             bold={activeIndex === index ? "medium" : undefined}
@@ -26,8 +27,9 @@ const List = ({ genres, activeIndex }: ListProps) => {
               "before:absolute before:-left-4 before:bottom-0 before:h-full before:content-['']",
               "before:border-l-[3px] before:border-l-primary-main before:brightness-50 ",
               /** after */
-              "after:absolute after:-left-[21px] after:top-1/3 after:h-3 after:w-3 after:rounded-full after:content-['']",
-              "after:bg-primary-main after:brightness-[var(--brightness-percentage)] "
+              "after:absolute after:-left-[21px] after:top-1/3 after:rounded-full after:content-['']",
+              "after:h-[var(--bullet-size)] after:w-[var(--bullet-size)] after:transition-all",
+              "after:bg-primary-main after:brightness-[var(--brightness-percentage)]"
             )}
           >
             {item}
