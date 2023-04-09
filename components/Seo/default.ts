f2c24f1d210756daad447cee603d23f1d884dcb8
lintang digdoyo <digdoyo.lintang@gmail.com>
2023-04-09 15:27:47 +0700

    feat: add meta tags

---
+++ components/Seo/default.ts
@@ -0,0 +1,49 @@
+export const defaultMeta = {
+  title: "Spotify",
+  siteName: "Spotify",
+  description: "Spotify landing page",
+  url: "http://localhost:3000",
+  type: "website",
+  robots: "follow, index",
+  themeColor: "#121212",
+  image: "http://localhost:3000/meta/meta.jpg",
+};
+
+export const favicons: Array<React.ComponentPropsWithoutRef<"link">> = [
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/favicon/apple-touch-icon.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon/favicon-16x16.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "96x96",
+    href: "/favicon/favicon-96x96.png",
+  },
+  {
+    rel: "manifest",
+    href: "/favicon/site.webmanifest",
+  },
+  {
+    rel: "mask-icon",
+    href: "/favicon/safari-pinned-tab.svg",
+    color: "#000000",
+  },
+  {
+    rel: "icon",
+    href: "/favicon.ico",
+  },
+];
---
+++ components/Seo/index.tsx
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import { defaultMeta, favicons } from "./default";
+
+interface SeoProps extends Partial<typeof defaultMeta> {
+  title?: string;
+}
+
+const Seo = (props: SeoProps) => {
+  const router = useRouter();
+
+  const meta = {
+    ...defaultMeta,
+    ...props,
+  };
+  meta.title = props.title ? `${props.title} | ${meta.siteName}` : meta.title;
+
+  return (
+    <Head>
+      <title>{meta.title}</title>
+      <meta name="robots" content={meta.robots} />
+      <meta name="description" content={meta.description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="canonical" href={`${meta.url}${router.asPath}`} />
+      {/* open graph */}
+      <meta property="og:url" content={`${meta.url}${router.asPath}`} />
+      <meta property="og:type" content={meta.type} />
+      <meta property="og:site_name" content={meta.siteName} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:title" content={meta.title} />
+      <meta property="og:image" content={meta.image} />
+      {/* twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:description" content={meta.description} />
+      <meta name="twitter:image" content={meta.image} />
+      {/* favicons */}
+      {favicons.map((linkProps) => (
+        <link key={linkProps.href} {...linkProps} />
+      ))}
+      <meta name="msapplication-TileColor" content={meta.themeColor} />
+      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="theme-color" content={meta.themeColor} />
+    </Head>
+  );
+};
+
+export default Seo;
